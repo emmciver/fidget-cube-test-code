d0131a66143470c5c4c6b29ab498ce5eee9aa019
emmciver <111801716+emmciver@users.noreply.github.com>
2022-08-31 12:29:20 -0600

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,35 +1,84 @@
+let Hand = 0
 input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index < 5; index++) {
+    for (let index = 0; index < 3; index++) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            . . . . .
+            # # # # #
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(1000)
         basic.showLeds(`
             # # # # #
-            # . . . #
-            # . . . #
-            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(500)
+        basic.showLeds(`
             # # # # #
-            `)
-        basic.pause(500)
-        basic.showLeds(`
-            . . . . .
-            . # # # .
-            . # . # .
-            . # # # .
-            . . . . .
-            `)
-        basic.pause(500)
-        basic.showLeds(`
             . . . . .
             . . . . .
-            . . # . .
             . . . . .
             . . . . .
             `)
-        basic.pause(500)
+        basic.pause(1000)
         basic.showLeds(`
             . . . . .
-            . # # # .
-            . # . # .
-            . # # # .
+            # # # # #
             . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
             `)
         basic.pause(500)
     }
@@ -38,5 +87,33 @@ input.onGesture(Gesture.ScreenDown, function () {
     for (let index = 0; index < 3; index++) {
         music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.UntilDone)
         basic.pause(1000)
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    Hand = randint(1, 3)
+    if (Hand == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (Hand == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
     }
 })
